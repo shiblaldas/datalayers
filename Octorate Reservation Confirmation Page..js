03ec48358397c7d8cfee657dba461c7294a671bf
Shib Lal Das <74951410+shiblaldas@users.noreply.github.com>
2025-08-05 19:32:35 +0600

    Octorate Reservation Confirmation Page..js

--- Octorate Reservation Confirmation Page..js
+++ Octorate Reservation Confirmation Page..js
@@ -2,14 +2,15 @@
   dataLayer.push({ ecommerce: null }); // Clear previous ecommerce data
 
   dataLayer.push({
-    event: "purchase",
+    event: "sld_purchase",
     ecommerce: {
       transaction_id: octorateReservation.refer,
       value: octorateReservation.total,
       currency: octorateReservation.currency,      
-      checkin: octorateReservation.checkin,
-      checkout: octorateReservation.checkout,
+      start_date: octorateReservation.checkin,
+      end_date: octorateReservation.checkout,
       nights: octorateReservation.nights,
+      people : octorateReservation.pax,     
       email: octorateReservation.email,
       phone: octorateReservation.phone,
       firstName: octorateReservation.firstName,
